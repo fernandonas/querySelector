4b1b2ad5887999f7e1ff242646703bd74e54319a
Fernando Augusto do Nascimento <fernando.nascimento@ambevtech.com.br>
2022-09-26 22:37:47 -0300

    Adicionado testes.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +12,48 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'querySelector'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('querySelector');
+  // Dois tipos:
+  // querySelector : Traz o primeiro elemento que encontra no HTML
+  // querySelectorAll : Traz todos os elementos que encontra no HTML
+
+
+  it('Buscar a primeira div', () => {
+    const element = fixture.nativeElement.querySelector('div').innerText;
+    console.log("Primeira div", element);
+    expect(element).toEqual('Formulário');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('querySelector app is running!');
+  it('Buscar todas as divs', () => {
+    const element = fixture.nativeElement.querySelectorAll("div");
+    console.log("Todas as divs", element);
+    expect(app).toBeTruthy();
+  });
+
+  it('Buscar elementos por css', () => {
+    const element = fixture.nativeElement.querySelector(".container").innerText;
+    console.log("Por css", element);
+    expect(element).toEqual('Enviar');
+  });
+
+  it('Buscar elementos por id', () => {
+    const element = fixture.nativeElement.querySelector("#footer").innerText;
+    console.log("Por id", element);
+    expect(element).toEqual('Rodapé');
+  });
+
+  it('Simular click', () => {
+    spyOn(app, 'changeText').and.callThrough();
+    const element = fixture.nativeElement.querySelector(".send-button");
+    element.click();
+    expect(app.changeText).toHaveBeenCalled();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'querySelector';
+  text = '';
+  value = '';
+
+  changeText(): void {
+    this.text = this.value;
+  }
 }
